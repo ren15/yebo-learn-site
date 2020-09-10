35e039de97517366126a4170aa3ec7801fdc62a6
Ильгам Курбанов <Ilgam.Kurbanov@NPPGKS.com>
2020-09-10 21:50:24 +0300

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
 @@include('cssWebp.js')
 @@include('burgerMenu.js')
 @@include('nav-menu.js')
+@@include('slider.js')
--- yebo-learn-site/js/script.js
+++ yebo-learn-site/js/script.js
@@ -62,3 +62,33 @@ window.addEventListener('resize',function(){
          document.querySelector('.menu').style.padding = '60px 0px'
     }
 });
+
+const slider = document.querySelector('.slider')
+
+const sliderElements = document.querySelectorAll('.slider__element')
+const buttonList = document.querySelector('.button__list')
+
+const changeSlide = (event) => {
+  let sliderWidth = document.querySelector('.slider__element').offsetWidth
+  const index = event.target.tabIndex
+  const link = document.querySelectorAll('.button__list li')
+  sliderWidth = (sliderWidth * index)
+  console.log(sliderWidth)
+  link.forEach((el, i) => {
+    
+    if (index == i && !el.classList.contains('active')) {
+      el.classList.add('active')
+    }
+    else {
+      el.classList.remove('active')
+    } 
+    sliderElements[i].style.left = `-${sliderWidth}px`
+  }) 
+}
+sliderElements.forEach((el, i)=>{
+  const li = document.createElement('li')
+  buttonList.appendChild(li)
+  li.addEventListener("click", changeSlide)
+  li.tabIndex = i
+  const act = (i==0) ? (() => li.className = "active")() : ''
+})
