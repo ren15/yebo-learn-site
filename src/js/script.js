0fc2c923a7cefdfd9b4ba918182dbf6cbb4273ea
Ильгам Курбанов <Ilgam.Kurbanov@NPPGKS.com>
2020-09-11 22:09:17 +0300

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,5 @@
 @@include('cssWebp.js')
 @@include('burgerMenu.js')
-<<<<<<< HEAD
 @@include('nav-menu.js')
 @@include('slider.js')
-=======
 @@include('nav-menu.js')
->>>>>>> a7589507affcbe6e8a14f6a69d519e3861668532
--- yebo-learn-site/js/script.js
+++ yebo-learn-site/js/script.js
@@ -62,7 +62,6 @@ window.addEventListener('resize',function(){
          document.querySelector('.menu').style.padding = '60px 0px'
     }
 });
-<<<<<<< HEAD
 
 const slider = document.querySelector('.slider')
 
@@ -93,5 +92,40 @@ sliderElements.forEach((el, i)=>{
   li.tabIndex = i
   const act = (i==0) ? (() => li.className = "active")() : ''
 })
-=======
->>>>>>> a7589507affcbe6e8a14f6a69d519e3861668532
+function getCoords(elem) {
+    var box = elem.getBoundingClientRect();
+    return {
+      top: box.top + pageYOffset,
+      left: box.left + pageXOffset
+    };
+  }
+
+window.addEventListener('scroll', function() {
+const coordY = getCoords(document.querySelector('#description-main')).top
+    if (window.innerWidth > 440) {
+        document.querySelector('.menu').style.backgroundColor = 'transparent ';
+        document.querySelector('.menu').style.backgroundPosition = 'center 35px'
+        document.querySelector('.menu').style.padding = '60px 0px'
+        if (window.pageYOffset >= coordY) {
+            document.querySelector('.menu').style.backgroundColor = 'rgba(247, 247, 247, 0.7)';
+            document.querySelector('.menu').style.backgroundPosition = 'center'
+            document.querySelector('.menu').style.padding = '40px 0px'
+        }
+    } else {
+        document.querySelector('.menu').style.background = 'url(/img/Logo-menu.png) center 35px no-repeat';
+        if (window.pageYOffset >= coordY) {
+            document.querySelector('.menu').style.background = 'transparent';
+
+        }
+    }
+  });
+
+window.addEventListener('resize',function(){
+    if (window.innerWidth < 440) {
+        document.querySelector('.menu').style.padding = '0px'
+    } else {
+         document.querySelector('.menu').style.background = 'url(/img/Logo-menu.png) center 35px no-repeat';
+         document.querySelector('.menu').style.backgroundPosition = 'center 35px'
+         document.querySelector('.menu').style.padding = '60px 0px'
+    }
+});
